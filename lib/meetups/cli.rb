d5f0d8a80c33bed714e4c90aeb522cfbfa6f6c10
KerryAlsace <kerrydougherty@gmail.com>
2016-08-17 16:18:33 -0400

    build out #menu method

--- lib/meetups/cli.rb
+++ lib/meetups/cli.rb
@@ -15,7 +15,23 @@ class Meetups::CLI
   end
 
   def menu
-    puts "Enter the number of a meetup to see more details"
+    input = nil
+    while input != "exit"
+      puts "Enter the number of a meetup to see more details, or type list to see the list of meetups, or type exit."
+      input = gets.strip
+      case input
+      when "1"
+        puts "More info on 1..."
+      when "2"
+        puts "More info on 2..."
+      when "list"
+        list_meetups
+      when "exit"
+        return
+      else
+        puts "Not sure what you want"
+      end
+    end
   end
 
   def goodbye
