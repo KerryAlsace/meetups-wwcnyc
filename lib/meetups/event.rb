6f27b81aded60176886c42d103c91380233184c0
KerryAlsace <kerrydougherty@gmail.com>
2016-08-17 17:20:18 -0400

    scrape additional details for meetups

--- lib/meetups/event.rb
+++ lib/meetups/event.rb
@@ -1,43 +1,31 @@
 class Meetups::Event
-  attr_accessor :name, :date, :time, :location, :url
+  attr_accessor :name, :date, :time, :location, :address, :url
 
   def self.upcoming
     #return upcoming WWCNYC meetups
-    event_1 = self.new
-    event_1.name = "#HIREWWCode- How to Negotiate your Salary with a Corporation"
-    event_1.date = "Tue Aug 23"
-    event_1.time = "6:30 PM"
-    event_1.location = "250 Greenwich St, New York, NY"
-    event_1.url = "https://www.meetup.com/WomenWhoCodeNYC/events/232116881/"
-
-    event_2 = self.new
-    event_2.name = "Holiday - Labor Day"
-    event_2.date = "Mon Sep 5"
-    event_2.time = "7:00 PM"
-    event_2.location = "USA, New York, NY"
-    event_2.url = "https://www.meetup.com/WomenWhoCodeNYC/events/228581253/"
-
-    [event_1, event_2]
     scrape_meetups
   end
 
   def self.scrape_meetups
     doc = Nokogiri::HTML(open("https://www.meetup.com/WomenWhoCodeNYC/"))
-    binding.pry
 
     event_1 = self.new
-    event_1.name = doc.css("ul.event-list li.event-item h3.flush--bottom span").first.text
-    event_1.date = doc.css("ul.event-list li.event-item li.dateTime span.date").first.text
-    event_1.time = doc.css("ul.event-list li.event-item li.dateTime span.time").first.text
-    event_1.location = "250 Greenwich St, New York, NY"
-    event_1.url = "https://www.meetup.com/WomenWhoCodeNYC/events/232116881/"
+    event_1.name = doc.css("ul.event-list li.event-item h3.flush--bottom span")[0].text
+    event_1.date = doc.css("ul.event-list li.event-item li.dateTime span.date")[0].text
+    event_1.time = doc.css("ul.event-list li.event-item li.dateTime span.time")[0].text
+    event_1.location = doc.css("ul.event-list li.event-item div.event-content dt.event-venuename a.no-color")[0].text
+    event_1.address = doc.css("ul.event-list li.event-item div.event-content dd.text--secondary")[0].text.split.join(" ").gsub(" (map)", "")
+    event_1.url = doc.css("ul.event-list li.event-item h3.flush--bottom a")[0].attr('href')
 
     event_2 = self.new
     event_2.name = doc.css("ul.event-list li.event-item h3.flush--bottom span")[1].text
     event_2.date = doc.css("ul.event-list li.event-item li.dateTime span.date")[1].text
     event_2.time = doc.css("ul.event-list li.event-item li.dateTime span.time")[1].text
-    event_2.location = "USA, New York, NY"
-    event_2.url = "https://www.meetup.com/WomenWhoCodeNYC/events/228581253/"
+    event_2.location = doc.css("ul.event-list li.event-item div.event-content dt.event-venuename a.no-color")[1].text
+    event_2.address = doc.css("ul.event-list li.event-item div.event-content dd.text--secondary")[1].text.split.join(" ").gsub(" (map)", "")
+    event_2.url = doc.css("ul.event-list li.event-item h3.flush--bottom a")[1].attr('href')
+
+    puts [event_1, event_2]
   end
       
 end
